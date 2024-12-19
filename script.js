537d8b4a24806d2291e8dd47204986194b708aaf
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-18 21:26:59 -0300

    finalizacao projeto cm arrays

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ let existentes = [];
 function adicionar(){
     var num = document.querySelector('#txtnum')
     var lista = document.querySelector('#lstnum');
-    var numValue = Number(num.value);
+    var res = document.querySelector('#res');
 
+
+    var numValue = Number(num.value);
     if(numValue <= 0 || numValue > 100 || isNaN(numValue)){
         alert('Por favor, insira um número válido.');
         num.value = "";
+        num.focus();
     }else{
         if(existentes.includes(numValue)){
             alert('Este número já existe na lista.');
             num.value = ''; // limpa input
+            num.focus();
         }else{
             existentes.push(numValue);
             num.value = ''; // limpa input
+            num.focus();
             while(true){
                 var item = document.createElement('option');
                 item.text = `Valor ${numValue} foi adicionado`;
@@ -27,5 +32,19 @@ function adicionar(){
 }
 
 function finalizar(){
-    item.innerHTML = 'Finalizar';
+    if (existentes.length == 0){
+        alert('A lista está vazia.');
+    }else{
+        let total = existentes.length;
+
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Total de valores adicionados: ${total}</p>`;
+        res.innerHTML += `<p>Maior valor: ${Math.max(...existentes)}</p>`;
+        res.innerHTML += `<p>Menor valor: ${Math.min(...existentes)}</p>`;
+        res.innerHTML += `<p>Soma dos valores: ${existentes.reduce((a, b) => a + b, 0)}</p>`;
+        res.innerHTML += `<p>Média dos valores: ${existentes.reduce((a, b) => a + b, 0) / total}</p>`;
+        res.innerHTML += `<p>Valores em ordem crescente: ${existentes.sort((a, b) => a - b).join(', ')}</p>`;
+        existentes = []; // limpa array existentes para nova lista
+    }
 }
